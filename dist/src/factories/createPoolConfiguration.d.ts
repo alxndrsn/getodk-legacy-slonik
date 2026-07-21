84a0e723708f9c9074ca1af44fd5895662efb62f
alxndrsn <alxndrsn>
2026-07-21 09:51:49 +0000

    remove dist directory

--- dist/src/factories/createPoolConfiguration.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ConnectionOptions } from 'pg-connection-string';
-import type { ClientConfigurationType } from '../types';
-export declare const createPoolConfiguration: (connectionUri: string, clientConfiguration: ClientConfigurationType) => ConnectionOptions;
