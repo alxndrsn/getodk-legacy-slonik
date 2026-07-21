f15100ff2b37a4809d8521cc8a80855695bea234
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-07-21 09:54:43 +0000

    remove dist directory (#11)

--- dist/src/tokens.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const ArrayToken: unique symbol;
-export declare const BinaryToken: unique symbol;
-export declare const ComparisonPredicateToken: unique symbol;
-export declare const IdentifierToken: unique symbol;
-export declare const JsonToken: unique symbol;
-export declare const ListToken: unique symbol;
-export declare const SqlToken: unique symbol;
-export declare const UnnestToken: unique symbol;
